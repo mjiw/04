b0c568ba246f0f383b6665e37c8e36a49c3abd16
mjiw <113112060+mjiw@users.noreply.github.com>
2022-09-27 17:21:06 +0900

    test6

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int year=0;
-	printf("input the year :");
-	scanf("%d",&year);
+	int second=0;
+	printf("input the second :");
+	scanf("%d",&second);
 	
-	if ( (year%4==0 && year%100!=0) || year%400==0)
-		printf("%d is the leap year!\n",year);
-	else
-		printf("%d is not the leap year!\n",year);
+	int a=(second/60)/60;
+	int b=(second/60)-(((second/60)/60)*60);
+	int c=second%60;
 	
+	printf("The time for %d second is %d:%d:%d\n",second,a,b,c);
 	
 	return 0;
 }
